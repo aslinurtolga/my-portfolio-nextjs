7885345050a89a527945d601262c5938da4c06ff
AslinurTolga <aslinurtolga@gmail.com>
2023-01-28 13:31:02 +0300

    fix: background circles l and d mode style changed

--- components/BgCircle.tsx
+++ components/BgCircle.tsx
@@ -8,17 +8,18 @@ const BgCircle = (props: Props) => {
     <motion.div
       initial={{ opacity: 0 }}
       animate={{
-        opacity: [0.1, 0.3, 0.4, 0.8, 0.2],
+        opacity: [0.1, 0.3, 0.4, 0.8, 0.2, 1.0],
         scale: [1, 2, 2, 3, 1],
+        borderRadius: ["20%", "20%", "50%", "80%", "20%"],
       }}
       transition={{ duration: 2 }}
       className="relative flex justify-center items-center"
     >
-      <div className="absolute border border-[#55555 rounded-full h-[200px] w-[200px] mt-48 animate-ping " />
-      <div className="absolute border border-[#55555 rounded-full h-[300px] w-[300px] mt-48 animate-ping" />
-      <div className="absolute border border-[#55555 rounded-full h-[500px] w-[500px] mt-48 animate-ping" />
-      <div className="absolute border border-[#ca3131 opacity-20 rounded-full h-[650px] w-[650px] mt-48 animate-pulse" />
-      <div className="absolute border border-[#55555 rounded-full h-[800px] w-[800px] mt-48 animate-pulse" />
+      <div className="absolute border border-[#55555] rounded-full h-[200px] w-[200px] mt-48 animate-ping " />
+      <div className="absolute border border-[#555555] rounded-full h-[300px] w-[300px] mt-48 animate-ping" />
+      <div className="absolute border border-[#555555] rounded-full h-[500px] w-[500px] mt-48 animate-ping" />
+      <div className="absolute border border-indigo-700 dark:border-[#ca3131] opacity-20 rounded-full h-[650px] w-[650px] mt-48 animate-pulse" />
+      <div className="absolute border border-[#555555] rounded-full h-[800px] w-[800px] mt-48 animate-pulse" />
     </motion.div>
   );
 };
