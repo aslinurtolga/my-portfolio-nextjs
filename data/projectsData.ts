a423f5616bd7dac945d4331a824b02e6ca382481
Aslınur Tolga <99428542+aslinurtolga@users.noreply.github.com>
2024-11-11 17:43:31 +0300

    Update projectsData.ts

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,49 +1,82 @@
-export const projectsData = [
+export const skillsData = [
   {
-    img: "https://github.com/aslinurtolga/random-user-app/blob/main/randomuser.gif?raw=true",
-    name: "Random User App",
-    description:
-      "Project aims get information about Random User App via a Javascript React.",
-    url: "https://randomuserapp1.netlify.app/",
-    language: "React.JS",
-    github: "https://github.com/aslinurtolga/random-user-app",
+    name: "React",
+    img: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
   },
   {
-    img: "https://github.com/aslinurtolga/image-gallery/blob/main/src/components/Animation.gif?raw=true",
-    name: "Image Gallery",
-    description:
-      "Project aims get information about Language Cards App via a Javascript React.",
-    url: "https://aslinurtolga-language-cards.netlify.app/",
-    language: "React.JS",
-    github: "https://github.com/aslinurtolga/language-cards",
+    name: "Next.JS",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg",
   },
   {
-    img: "https://github.com/aslinurtolga/tourPlaces/blob/main/tourplaces.gif?raw=true",
-    name: "Tour Places",
-    description:
-      "A web application that I prepared to work with React JS using the Tour Places.",
-    url: "https://aslinurtolga.netlify.app/",
-    language: "React.JS",
-    github: "https://github.com/aslinurtolga/tourPlaces",
+    name: "JavaScript",
+    img: "https://upload.wikimedia.org/wikipedia/commons/6/64/JavaScript-logo.png",
   },
   {
-    img: "https://github.com/aslinurtolga/githubProfileAPI/blob/main/images/github2.gif?raw=true",
-
-    name: "Github Profile with Pure JS",
-    description:
-      "A web application made using Pure JavaScript that displays their account with GIFs by entering a github username.",
-    url: "https://aslinurtolga.github.io/githubProfileAPI/",
-    language: "JavaScript",
-    github: "https://github.com/aslinurtolga/githubProfileAPI",
+    name: "TypeScript",
+    img: "https://upload.wikimedia.org/wikipedia/commons/4/4e/TypeScript_Logo_2020.svg",
   },
   {
-    img: "https://github.com/aslinurtolga/reactjs-website-from-scratch/blob/main/src/assets/travel.gif?raw=true",
-
-    name: "SPX TRVL Reactjs Website From Scratch",
-    description:
-      "Project aims get information about SPX TRVL Reactjs Website From Scratch App via a Javascript React",
-    url: "https://reactjs-website-from-scratch.vercel.app/",
-    language: "React.js",
-    github: "https://github.com/aslinurtolga/reactjs-website-from-scratch",
+    name: "Redux",
+    img: "https://upload.wikimedia.org/wikipedia/commons/7/76/Redux_logo.png",
+  },
+  {
+    name: "Python",
+    img: "https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg",
+  },
+  {
+    name: "HTML",
+    img: "https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg",
+  },
+  {
+    name: "CSS",
+    img: "https://upload.wikimedia.org/wikipedia/commons/6/62/CSS3_logo.svg",
+  },
+  {
+    name: "SASS",
+    img: "https://upload.wikimedia.org/wikipedia/commons/9/96/Sass_Logo_Color.svg",
+  },
+  {
+    name: "Django",
+    img: "https://upload.wikimedia.org/wikipedia/commons/7/75/Django_logo.svg",
+  },
+  {
+    name: "Bootstrap",
+    img: "https://upload.wikimedia.org/wikipedia/commons/4/43/Bootstrap_4_logo.svg",
+  },
+  {
+    name: "TailwindCSS",
+    img: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg",
+  },
+  {
+    name: "Material UI",
+    img: "https://upload.wikimedia.org/wikipedia/commons/4/47/Material-UI-Logo.svg",
+  },
+  {
+    name: "Git",
+    img: "https://upload.wikimedia.org/wikipedia/commons/a/a7/Git-Logo.svg",
+  },
+  {
+    name: "GitHub",
+    img: "https://upload.wikimedia.org/wikipedia/commons/9/95/Logo_GitHub.png",
+  },
+  {
+    name: "Firebase",
+    img: "https://upload.wikimedia.org/wikipedia/commons/4/46/Firebase_Logo.svg",
+  },
+  {
+    name: "NPM",
+    img: "https://upload.wikimedia.org/wikipedia/commons/d/d2/Npm-logo.svg",
+  },
+  {
+    name: "VS Code",
+    img: "https://upload.wikimedia.org/wikipedia/commons/e/e5/Visual_Studio_Code_1.35_icon.svg",
+  },
+  {
+    name: "Storybook",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/storybook/storybook-original.svg",
+  },
+  {
+    name: "PostgreSQL",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg",
   },
 ];
