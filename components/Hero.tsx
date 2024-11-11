123c19531eb3478c46bcddb8255e2f7559069f1c
Aslınur Tolga <99428542+aslinurtolga@users.noreply.github.com>
2024-11-11 16:53:59 +0300

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,39 +19,39 @@ const Hero = (props: Props) => {
   });
 
   return (
-    <div className="h-screen flex flex-col items-center justify-center text-center space-y-8 overflow-hidden">
+    <div className="h-screen relative flex flex-col items-center justify-center text-center space-y-8 overflow-hidden bg-gradient-to-r from-indigo-500 via-purple-500 to-indigo-900">
       <BgCircle />
       <Image
         src="https://media.licdn.com/dms/image/v2/D4D03AQFrubCF18joHw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1701159993067?e=1736985600&v=beta&t=3i0kTqqn8NltyW1iRctp9MQcvoLIHhh27FWFF_Z-Nnk"
         alt="image"
-        width={130}
-        height={130}
+        width={150}
+        height={150}
         loading="lazy"
-        className="rounded-full"
+        className="rounded-full border-4 border-indigo-700 shadow-2xl"
       />
-      <div className="z-20">
-        <h2 className="text-sm uppercase text-gray-700 dark:text-gray-300 pb-3 tracking-[12px]">
+      <div className="z-20 text-white">
+        <h2 className="text-xl uppercase text-gray-200 dark:text-gray-300 pb-3 tracking-[8px] font-semibold">
           {title}
           <Cursor cursorColor="#ca3131" />
         </h2>
-        <h1 className="text-4xl text-indigo-900 dark:text-red-500 lg:text-6xl font-semibold px-8">
+        <h1 className="text-4xl lg:text-6xl font-extrabold px-8 text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 via-purple-600 to-indigo-800">
           <span>{text}</span>
           <Cursor cursorColor="#ca3131" />
         </h1>
 
-        <div className="pt-6 ">
+        <div className="pt-8 space-x-6">
           <Link href="#about">
-            <button className="heroButton text-[#626974] hover:text-black dark:text-gray-100 dark:hover:text-gray-500">
+            <button className="heroButton transition-colors duration-300 text-[#f0f0f0] hover:text-white bg-transparent hover:bg-indigo-700 dark:text-gray-100 dark:hover:bg-indigo-600 py-2 px-6 rounded-lg text-lg font-medium">
               About
             </button>
           </Link>
           <Link href="#skills">
-            <button className="heroButton text-[#626974]  hover:text-black dark:text-gray-100 dark:hover:text-gray-500">
+            <button className="heroButton transition-colors duration-300 text-[#f0f0f0] hover:text-white bg-transparent hover:bg-indigo-700 dark:text-gray-100 dark:hover:bg-indigo-600 py-2 px-6 rounded-lg text-lg font-medium">
               Skills
             </button>
           </Link>
           <Link href="#projects">
-            <button className="heroButton text-[#626974] hover:text-black dark:text-gray-100 dark:hover:text-gray-500">
+            <button className="heroButton transition-colors duration-300 text-[#f0f0f0] hover:text-white bg-transparent hover:bg-indigo-700 dark:text-gray-100 dark:hover:bg-indigo-600 py-2 px-6 rounded-lg text-lg font-medium">
               Projects
             </button>
           </Link>
